b43dd24c53380069e42b0aa9dff30eea4eebcdec
nguyendd123 <nguyendangdung1122007@gmail.com>
2024-11-30 16:30:28 +0700

    add allowjs to tsconfig and test db

--- backend/src/controllers/models/Storm.ts
+++ backend/src/controllers/models/Storm.ts
@@ -1,7 +1,15 @@
 import mongoose, { Schema } from "mongoose";
 
 const Storm = new mongoose.Schema ({
+    gdacs_id : String,
     name : String,
+    from_to : String,
+    exposed_countries : String,
+    exposed_population : String,
+    maximum_wind_speed : String,
+    maximum_storm_surge : String,
+    vunerability : String,
+    gdacs_score : Number
 })
 
 const storm = mongoose.model ('storm', Storm)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello")
+import Express from "express" 
+import mongoose from "mongoose"
+
+const app = Express()
+mongoose.connect (process.env.CURRENT_STORMS_DB_URL as string)
+    .then(() => console.log ("Connect successfully !!!!"))
+    .catch (() => console.log ("you dumb ass"))
+
+
+app.get ('/', (req, res) => {
+    res.send ("HELLO WORLD")
+})
+
+app.listen (process.env.PORT)
+
+
+
