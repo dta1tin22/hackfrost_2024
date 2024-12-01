bbdda5893d9c01b285a84baff14f8f1268b61f05
WwOwW4fun <xuanan437@gmail.com>
2024-12-01 14:35:24 +0700

    Add new files

--- backend/src/routes/SendData.ts
+++ backend/src/routes/SendData.ts
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 import { Express } from 'express';
 import { SendData } from '../controllers/SendDataToDB';
 
 export const SendDataFromAPI = (app: Express) => {
 	app.post('/', SendData);
 };
+=======
+import {Express} from "express"
+import express from "express"
+import {SendJSON} from "../controllers/SendJSON"
+
+export const SendDataFromAPI = (app : Express) => {
+    app.use(express.json())
+    app.post('/send', SendJSON)
+}
+>>>>>>> 75e4506 (Add new files)
--- backend/src/routes/stormByID.ts
+++ backend/src/routes/stormByID.ts
@@ -1,6 +1,11 @@
 import { GetStormById } from '../controllers/GetParticularStorm';
 import { Express } from 'express';
 
+<<<<<<< HEAD
 export const FindStormByID = (app: Express) => {
 	app.get('/:_id', GetStormById);
 };
+=======
+
+
+>>>>>>> 75e4506 (Add new files)
