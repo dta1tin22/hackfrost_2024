70e3b08da4d304ed59e7997028abfac4ef36f330
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-02 00:30:15 +0700

    Connect frontend and backend

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -13,7 +13,6 @@ export interface StormInformation {
 	vulnerability: string;
 	gdacs_score: number;
 }
-<<<<<<< HEAD
 
 export const searchStorms = async (value: string) => {
 	const response = await fetch(`${PUBLIC_SERVER_URL}/search?value=${value}`)
@@ -22,5 +21,3 @@ export const searchStorms = async (value: string) => {
 
 	return data.stormsInformation as StormInformation[];
 }
-=======
->>>>>>> e35faef (Format code)
