76c441ec66dae20e2d336e2cdb3ae88207b1de53
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-07 22:12:57 +0700

    Format code

--- frontend/src/components/StormDataTable.svelte
+++ frontend/src/components/StormDataTable.svelte
@@ -10,47 +10,23 @@
 	let { data }: StormDataTableProps = $props();
 </script>
 
-{#snippet calendarIcon()}
-	<Calendar />
-{/snippet}
-
-{#snippet cloudIcon()}
-	<Cloud />
-{/snippet}
-
-{#snippet peopleIcon()}
-	<Users />
-{/snippet}
-
-{#snippet windIcon()}
-	<Wind />
-{/snippet}
-
-{#snippet wavesIcon()}
-	<Waves />
-{/snippet}
-
-{#snippet alertIcon()}
-	<AlertTriangle />
-{/snippet}
-
 <div class="grid grid-cols-2 gap-6">
-	<StormInfoCard icon={calendarIcon} title="Date Range">
+	<StormInfoCard Icon={Calendar} title="Date Range">
 		<p class="text-lg">{data.from_to}</p>
 	</StormInfoCard>
-	<StormInfoCard icon={cloudIcon} title="Exposed Countries">
+	<StormInfoCard Icon={Cloud} title="Exposed Countries">
 		<p class="text-lg">{data.exposed_countries.join(', ')}</p>
 	</StormInfoCard>
-	<StormInfoCard icon={peopleIcon} title="Exposed Population">
+	<StormInfoCard Icon={Users} title="Exposed Population">
 		<p class="text-lg">{data.exposed_population}</p>
 	</StormInfoCard>
-	<StormInfoCard icon={windIcon} title="Maximum Wind Speed">
+	<StormInfoCard Icon={Wind} title="Maximum Wind Speed">
 		<p class="text-lg">{data.maximum_wind_speed}</p>
 	</StormInfoCard>
-	<StormInfoCard icon={wavesIcon} title="Maximum Storm Surge">
+	<StormInfoCard Icon={Waves} title="Maximum Storm Surge">
 		<p class="text-lg">{data.maximum_storm_surge}</p>
 	</StormInfoCard>
-	<StormInfoCard icon={alertIcon} title="Vulnerability">
+	<StormInfoCard Icon={AlertTriangle} title="Vulnerability">
 		<p class="text-lg">{data.vulnerability}</p>
 	</StormInfoCard>
 </div>
--- frontend/src/components/StormInfoCard.svelte
+++ frontend/src/components/StormInfoCard.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
+	import type { Icon as IconType } from 'lucide-svelte';
 	import type { Snippet } from 'svelte';
 
 	interface StormInfoCardProps {
-		icon: Snippet;
+		Icon: typeof IconType;
 		title: string;
 		children: Snippet;
 	}
 
-	const { icon, title, children }: StormInfoCardProps = $props();
+	const { Icon, title, children }: StormInfoCardProps = $props();
 </script>
 
 <div class="rounded-lg p-4 shadow-md bg-slate-300 space-y-4">
 	<div class="flex items-center space-x-2">
-		{@render icon()}
+		<Icon />
 		<h2 class="text-2xl font-semibold">{title}</h2>
 	</div>
 	<div>
